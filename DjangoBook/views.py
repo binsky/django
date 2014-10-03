4ed82f1efde6c666a1943bd23a9949bc34fdd885
binsky <gobrol@gmail.com>
2014-10-03 09:44:28 +0300

    Little changes

--- DjangoBook/views.py
+++ DjangoBook/views.py
@@ -1,4 +1,4 @@
-# from django.http import HttpResponse
+#from django.http import HttpResponse
 #from django.template.loader import get_template
 #from django.template import Context
 from django.http import Http404, HttpResponse, HttpResponseRedirect
